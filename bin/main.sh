b240612d8863e46ac4cc89b6615c037313ea2d58
affinis <affinis@sjtu.edu.cn>
2021-12-20 08:47:29 +0800

    new import

---
+++ bin/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#set -e
+#set -u
+
+source ./configs.conf
+
+bash spacewrapper.sh $A $A_image
+bash spacewrapper.sh $B $B_image
+bash spacewrapper.sh $C $C_image
+bash spacewrapper.sh $D $D_image
